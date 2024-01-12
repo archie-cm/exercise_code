432f1aa72c8e5fe75cd85188d7287806814d4d42
Archie Citra Muhammad <108534539+archie-cm@users.noreply.github.com>
2024-01-12 14:31:31 +0700

    Update javaPath.java

--- javaPath.java
+++ javaPath.java
@@ -74,6 +74,24 @@ public class Bee {
   }
 } 
 
+
+import java.util.ArrayList;
+
+public class BeeHive {
+  private ArrayList<Bee> beeList;
+
+  public BeeHive(int numBees) {
+    this.beeList = new ArrayList<Bee>();
+    for (int i = 0; i < numBees; i++) {
+      this.beeList.add(new Bee("bee" + i));
+    }
+  }
+
+  public static void main(String[] args) {
+    BeeHive myHive = new BeeHive(365);
+  }
+}
+
 # exercise
 
 public void printBees() {
