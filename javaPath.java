9e36bcdbf4e8dcb921a986f2507c68b1dbee57f7
Archie Citra Muhammad <108534539+archie-cm@users.noreply.github.com>
2024-01-15 21:52:41 +0700

    add Iteration with Recursion

--- javaPath.java
+++ javaPath.java
@@ -152,3 +152,21 @@ public static int largestColumn(int[][] arr2D) {
     return largestColumn;
 } 
 
+
+public static String reverseString(String text){
+  // base case
+  if (text.length() == 0) {
+    return text;
+  } else {
+    // recursive call
+    return reverseString(text.substring(1)) + text.charAt(0);
+  }
+}
+
+public static void main(String[] args) {
+  String str = new String("howdy");
+  // calling recursive function
+  String reverse = reverseString(str);
+  System.out.println(reverse); // Prints: ydwoh
+}
+
