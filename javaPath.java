398a9e77f4f40689bbffda57ef7e6387cdd2e6f5
Archie Citra Muhammad <108534539+archie-cm@users.noreply.github.com>
2024-01-17 16:53:30 +0700

    add recursion binary search

--- javaPath.java
+++ javaPath.java
@@ -170,3 +170,46 @@ public static void main(String[] args) {
   System.out.println(reverse); // Prints: ydwoh
 }
 
+// binary search
+
+class Main
+{
+  public static int binarySearch(int[] arr, int left, int right, int target)
+  {
+   
+    int mid = left + (right - left) / 2;
+
+    // Base condition (key value is found)
+    if (target == arr[mid]) {
+      return mid;
+    }
+
+    // discard all elements in the right search space
+    // including the mid element
+    if (target < arr[mid]) {
+      return binarySearch(arr, left, mid - 1, target);
+    }
+
+    // discard all elements in the left search space
+    // including the mid element
+     if (target > arr[mid]) {
+      return binarySearch(arr, mid + 1, right, target);
+    }
+  }
+
+  public static void main(String[] args)
+  {
+    int[] arr = { 2, 5, 6, 8, 9, 10 };
+    int target = 8;
+ 
+    int left = 0;
+    int right = arr.length - 1;
+ 
+    int index = binarySearch(arr, left, right, target);
+   if (index != -1) {
+      System.out.println("Element found at index " + index);
+    } else {
+      System.out.println("Element not found in the array");
+    }
+  }
+}
