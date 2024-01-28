d2462df7e902c2358d567c913991fb99709d178a
Archie Citra Muhammad <108534539+archie-cm@users.noreply.github.com>
2024-01-28 21:07:12 +0700

    add gradebook

--- data_engineer_path.py
+++ data_engineer_path.py
@@ -114,3 +114,45 @@ print(customer_data)
 customer_data_final = customer_data + [["Amit", "Large", True], ["Karim", "X-Large", False]]
 
 print(customer_data_final)
+
+# gradebook
+
+last_semester_gradebook = [["politics", 80], ["latin", 96], ["dance", 97], ["architecture", 65]]
+
+# Your code below: 
+subject = [
+  "physics",
+"calculus",
+"poetry",
+"history"
+]
+
+grades = [98, 97, 85, 88]
+
+gradebook = [
+  ["physics",	98],
+["calculus",	97],
+["poetry",	85],
+["history",	88]
+]
+
+print(gradebook)
+
+gradebook.append(["computer science", 100])
+
+gradebook.append(["visual arts", 93])
+
+gradebook[-1][-1] = 97
+
+print(gradebook)
+
+gradebook[2].remove(85)
+
+gradebook[2].append("Pass")
+
+print(gradebook)
+
+full_gradebook = last_semester_gradebook + gradebook 
+
+print(full_gradebook)
+
