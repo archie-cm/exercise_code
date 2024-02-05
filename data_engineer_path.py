bf73b4b9ec9bb06c9de75b04f133ec43ddf158e8
Archie Citra Muhammad <108534539+archie-cm@users.noreply.github.com>
2024-02-05 21:51:48 +0700

    add list comprehensions with conditionals

--- data_engineer_path.py
+++ data_engineer_path.py
@@ -156,3 +156,9 @@ full_gradebook = last_semester_gradebook + gradebook
 
 print(full_gradebook)
 
+#  list comprehensions with conditionals
+numbers = [2, -1, 79, 33, -45]
+
+no_if   = [num * 2 for num in numbers]
+if_only = [num * 2 for num in numbers if num < 0]
+if_else = [num * 2 if num < 0 else num * 3 for num in numbers]
