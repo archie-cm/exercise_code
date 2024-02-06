4d2c1caf3eba6b11ac423a4942a925f0bfb88b81
Archie Citra Muhammad <108534539+archie-cm@users.noreply.github.com>
2024-02-06 13:53:59 +0700

    add Data Analyst at Carly’s Clippers

--- data_engineer_path.py
+++ data_engineer_path.py
@@ -162,3 +162,40 @@ numbers = [2, -1, 79, 33, -45]
 no_if   = [num * 2 for num in numbers]
 if_only = [num * 2 for num in numbers if num < 0]
 if_else = [num * 2 if num < 0 else num * 3 for num in numbers]
+
+# Data Analyst at Carly’s Clippers
+hairstyles = ["bouffant", "pixie", "dreadlocks", "crew", "bowl", "bob", "mohawk", "flattop"]
+
+prices = [30, 25, 40, 20, 20, 35, 50, 35]
+
+last_week = [2, 3, 5, 8, 4, 4, 6, 2]
+
+total_price = 0
+
+for price in prices:
+  total_price += price
+
+average_price = total_price / len(prices)
+
+print("Average Haircut Prices:", average_price)
+
+new_prices = [price-5 for price in prices]
+
+print(new_prices)
+
+total_revenue = 0
+
+for i in range(len(hairstyles)):
+  total_revenue += (prices[i] * last_week[i])
+
+print("Total Revenue:", total_revenue)
+
+average_daily_revenue = total_revenue / 7
+
+print(average_daily_revenue)
+
+cuts_under_30 = [hairstyles[i] for i in range(len(new_prices)-1) if new_prices[i] < 30]
+
+print(cuts_under_30)
+
+
