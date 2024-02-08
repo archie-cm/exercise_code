28918a30fd7ecb4787712e712e90588083ba4bfb
Archie Citra Muhammad <108534539+archie-cm@users.noreply.github.com>
2024-02-08 16:20:25 +0700

    add physics class

--- data_engineer_path.py
+++ data_engineer_path.py
@@ -198,4 +198,43 @@ cuts_under_30 = [hairstyles[i] for i in range(len(new_prices)-1) if new_prices[i
 
 print(cuts_under_30)
 
+# physic class
+train_mass = 22680
+train_acceleration = 10
+train_distance = 100
+bomb_mass = 1
 
+
+# Write your code below: 
+def f_to_c(f_temp):
+  c_temp = (f_temp - 32) * 5/9
+  return c_temp
+
+def c_to_f(c_temp):
+  f_temp = c_temp * 9/5 + 32
+  return f_temp
+
+print(f_to_c(100))
+
+c0_in_fahrenheit = c_to_f(0)
+
+def get_force(mass, acceleration):
+  return mass * acceleration
+
+train_force = get_force(train_mass, train_acceleration)
+
+print("The GE train supplies "+ str(train_force) + " Newtons of force.")
+
+def get_energy(mass, c = 3*10**8):
+  return mass * (c**2)
+
+bomb_energy = get_energy(bomb_mass)
+
+print(bomb_energy)
+
+def get_work(mass, acceleration, distance):
+  return get_force(mass, acceleration) * distance
+
+train_work = get_work(train_mass, train_acceleration, train_distance)
+
+print("The GE train does " + str(train_work) +" Joules of work over "+ str(train_distance) + " meters.")
