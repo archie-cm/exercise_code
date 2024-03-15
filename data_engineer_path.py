004ece18deb08d5b8d2c60934fbd12c58fc87248
Archie Citra Muhammad <108534539+archie-cm@users.noreply.github.com>
2024-03-15 15:05:32 +0700

    add String

--- data_engineer_path.py
+++ data_engineer_path.py
@@ -408,3 +408,16 @@ def update_point_totals():
 
 update_point_totals()
 print(player_to_points)
+
+# String
+authors = "Audre Lorde,Gabriela Mistral,Jean Toomer,An Qi,Walt Whitman,Shel Silverstein,Carmen Boullosa,Kamala Suraiyya,Langston Hughes,Adrienne Rich,Nikki Giovanni"
+
+author_names = authors.split(',')
+
+print(author_names)
+
+author_last_names = []
+for name in author_names:
+  author_last_names.append(name.split()[-1])
+  
+print(author_last_names)
