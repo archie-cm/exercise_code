e6e6e8be2f77b474629ffdfc6416e29c4b8fc963
Archie Citra Muhammad <108534539+archie-cm@users.noreply.github.com>
2024-03-17 17:15:55 +0700

    add Hacking The Fender

--- data_engineer_path.py
+++ data_engineer_path.py
@@ -657,3 +657,49 @@ data_payload = [
 
 with open('data.json', 'w') as data_json:
   json.dump(data_payload, data_json)
+
+# Hacking The Fender
+import csv
+import json
+
+# fields = ['Username','Password']
+compromised_users=[]
+
+with open('passwords.csv',newline='') as password_file:
+  password_csv = csv.DictReader(password_file)
+  for row in password_csv:
+    # print(row['Password'])
+    compromised_users.append(row['Username'])
+
+with open('compromised_users.txt','w') as compromised_user_file:
+  for compromised_user in compromised_users:
+    compromised_user_file.write("\n"+ compromised_user)
+
+with open('boss_message.json','w') as boss_message:
+  boss_message_dict = {
+    "recipient": "The Boss", 
+    "message":"Mission Success"
+    }
+  json.dump(boss_message_dict, boss_message)
+
+with open('new_password.csv','w') as new_passwords_obj:
+  slash_null_sig = """
+   _  _     ___   __  ____             
+/ )( \   / __) /  \(_  _)            
+) \/ (  ( (_ \(  O ) )(              
+\____/   \___/ \__/ (__)             
+ _  _   __    ___  __ _  ____  ____  
+/ )( \ / _\  / __)(  / )(  __)(    \ 
+) __ (/    \( (__  )  (  ) _)  ) D ( 
+\_)(_/\_/\_/ \___)(__\_)(____)(____/ 
+        ____  __     __   ____  _  _ 
+ ___   / ___)(  )   / _\ / ___)/ )( \
+(___)  \___ \/ (_/\/    \\___ \) __ (
+       (____/\____/\_/\_/(____/\_)(_/
+ __ _  _  _  __    __                
+(  ( \/ )( \(  )  (  )               
+/    /) \/ (/ (_/\/ (_/\             
+\_)__)\____/\____/\____/
+"""
+
+  new_passwords_obj.write(slash_null_sig)
