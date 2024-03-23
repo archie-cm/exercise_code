5f065405a46bc867ca3c3553a76223681e49f6ac
Archie Citra Muhammad <108534539+archie-cm@users.noreply.github.com>
2024-03-23 14:33:42 +0700

    Update and rename data_engineer.sqlite to data_engineer.sql

--- data_engineer.sql
+++ data_engineer.sql
@@ -5,3 +5,14 @@ SELECT username, posts,
     RANGE BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING
   ) as 'most_post'
 FROM social_media;
+
+-- result
+-- username	posts	most_post
+-- aliaabhatt	5	25
+-- aliaabhatt	7	25
+-- aliaabhatt	9	25
+-- aliaabhatt	9	25
+-- aliaabhatt	9	25
+-- aliaabhatt	13	25
+-- aliaabhatt	14	25
+-- aliaabhatt	25	25
