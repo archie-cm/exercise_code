c733bda85251d36e831294f1d7a3168b34c9802c
Archie Citra Muhammad <108534539+archie-cm@users.noreply.github.com>
2024-04-04 22:24:18 +0700

    add pie chart

--- data_engineer_path.py
+++ data_engineer_path.py
@@ -818,3 +818,29 @@ ax.set_xticks(range(0, 10))
 ax.set_xticklabels(games, rotation=30)
 
 plt.show()
+
+
+# pie chart
+import codecademylib3_seaborn
+from matplotlib import pyplot as plt
+import numpy as np
+import pandas as pd
+
+labels = ["US", "DE", "CA", "N/A", "GB", "TR", "BR", "DK", "PL", "BE", "NL", "Others"]
+
+countries = [447, 66, 64, 49, 45, 28, 25, 20, 19, 17, 17, 279]
+
+colors = ['lightskyblue', 'gold', 'lightcoral', 'gainsboro', 'royalblue', 'lightpink', 'darkseagreen', 'sienna', 'khaki', 'gold', 'violet', 'yellowgreen']
+
+# Make your pie chart here
+
+explode = (0.1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+
+plt.pie(countries, explode=explode, colors=colors, shadow=True, startangle=345, autopct='%1.0f%%', pctdistance=1.15)
+
+plt.title("League of Legends Viewers' Whereabouts")
+
+plt.legend(labels, loc="right")
+
+plt.show()
+
