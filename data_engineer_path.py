cbd00342ed22d0a682a7c0b3b919f58704f33ef6
Archie Citra Muhammad <108534539+archie-cm@users.noreply.github.com>
2024-04-04 22:22:57 +0700

    add bar chart

--- data_engineer_path.py
+++ data_engineer_path.py
@@ -797,3 +797,24 @@ click_source_by_month_pivot = click_source_by_month.pivot(
 ).reset_index()
 
 print(click_source_by_month_pivot)
+
+
+# bar chart
+games = ["LoL", "Dota 2", "CS:GO", "DayZ", "HOS", "Isaac", "Shows", "Hearth", "WoT", "Agar.io"]
+
+viewers =  [1070, 472, 302, 239, 210, 171, 170, 90, 86, 71]
+
+plt.bar(range(len(games)), viewers, color='slateblue')
+
+plt.legend(["Twitch"])
+
+plt.xlabel('Games')
+plt.ylabel('Viewers')
+
+ax = plt.subplot()
+
+ax.set_xticks(range(0, 10))
+
+ax.set_xticklabels(games, rotation=30)
+
+plt.show()
